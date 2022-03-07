24c651da93ad213c402b0637bd8634f2e54968e8
gpbaculio <gpbaculio@gmail.com>
2022-03-07 11:23:26 +0800

    add optional children props

--- rnEcommerce/src/components/AnimatedDynamicElement.tsx
+++ rnEcommerce/src/components/AnimatedDynamicElement.tsx
@@ -3,9 +3,15 @@ import Animated from 'react-native-reanimated';
 
 class DynamicClassWrapper extends React.Component<{
   dynamicElement: React.ReactNode;
+  children?: React.ReactNode;
 }> {
   render(): React.ReactNode {
-    return this.props.dynamicElement;
+    return (
+      <>
+        {this.props.dynamicElement}
+        {!!this.props.children && this.props.children}
+      </>
+    );
   }
 }
 
